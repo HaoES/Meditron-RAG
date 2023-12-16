c4b7849907a352b5f1e77b9f795d2c16c023d048
HaoES <hamza.essamaali@gmail.com>
2023-12-16 13:53:18 +0100

    update

--- app.py
+++ app.py
@@ -2,14 +2,18 @@ import streamlit as st
 from pypdf import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import Qdrant
 from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.llms import HuggingFaceHub
+from langchain.chains.question_answering import load_qa_chain
+
 import os
 
 
 def get_vectors(chunks):
     embeddings = HuggingFaceEmbeddings(model_name="NeuML/pubmedbert-base-embeddings")
-    vectorstore = FAISS.from_texts(texts=chunks, embeddings=embeddings)
+    vectorstore = FAISS.from_texts(texts=chunks, embedding=embeddings)
     return vectorstore
 
 
@@ -30,10 +34,30 @@ def get_chunks(text):
     return chunks
 
 
+def handle_query(query):
+    vects = st.session_state.vects
+    docs = vects.similarity_search(query=query, k=3)
+    llm = HuggingFaceHub(
+        repo_id="mistralai/Mixtral-8x7B-Instruct-v0.1",
+        model_kwargs={"tempearture": 0.0, "max_length": 2048},
+    )
+    chain = load_qa_chain(llm=llm, chain_type="stuff")
+    response = chain.run(input_documents=docs, question=query)
+    st.write(response)
+
+
 def main():
     st.set_page_config(page_title="Med Prep", page_icon=":medical_symbol:")
     st.header("Med Prep :medical_symbol:")
-    st.text_input("Ask questions about your documents:")
+
+    # create session state object
+    if "vects" not in st.session_state:
+        st.session_state.vects = None
+
+    # receiving user's query
+    query = st.text_input("Ask questions about your documents:")
+    if query:
+        handle_query(query)
 
     with st.sidebar:
         st.subheader("Your documents")
@@ -49,7 +73,8 @@ def main():
                 # get chunks of texts
                 chunks = get_chunks(raw_text)
                 # get vectorstore
-                vectorstore.add_texts(chunks)
+                vects = get_vectors(chunks)
+                st.session_state.vects = vects
 
 
 if __name__ == "__main__":
